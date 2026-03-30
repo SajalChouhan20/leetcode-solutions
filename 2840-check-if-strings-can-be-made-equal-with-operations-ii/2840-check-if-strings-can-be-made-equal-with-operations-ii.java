801b3e5728a53e302fef339579ebdb84ff7826f5
SajalChouhan20 <chouhansajal9@gmail.com>
2026-03-31 00:44:23 +0530

    Time: 43 ms (14.47%), Space: 48 MB (65.79%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,29 +1,48 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[] even1 = new int[26];
-        int[] odd1 = new int[26];
-        int[] even2 = new int[26];
-        int[] odd2 = new int[26];
+        // int[] even1 = new int[26];
+        // int[] odd1 = new int[26];
+        // int[] even2 = new int[26];
+        // int[] odd2 = new int[26];
+
+        // for (int i = 0; i < s1.length(); i++) {
+        //     char c1 = s1.charAt(i);
+        //     char c2 = s2.charAt(i);
+
+        //     if (i % 2 == 0) {
+        //         even1[c1 - 'a']++;
+        //         even2[c2 - 'a']++;
+        //     } else {
+        //         odd1[c1 - 'a']++;
+        //         odd2[c2 - 'a']++;
+        //     }
+        // }
+
+        // for (int i = 0; i < 26; i++) {
+        //     if (even1[i] != even2[i] || odd1[i] != odd2[i]) {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+        Map<Character, Integer> even1 = new HashMap<>();
+        Map<Character, Integer> odd1 = new HashMap<>();
+        Map<Character, Integer> even2 = new HashMap<>();
+        Map<Character, Integer> odd2 = new HashMap<>();
 
         for (int i = 0; i < s1.length(); i++) {
             char c1 = s1.charAt(i);
             char c2 = s2.charAt(i);
 
             if (i % 2 == 0) {
-                even1[c1 - 'a']++;
-                even2[c2 - 'a']++;
+                even1.put(c1, even1.getOrDefault(c1, 0) + 1);
+                even2.put(c2, even2.getOrDefault(c2, 0) + 1);
             } else {
-                odd1[c1 - 'a']++;
-                odd2[c2 - 'a']++;
+                odd1.put(c1, odd1.getOrDefault(c1, 0) + 1);
+                odd2.put(c2, odd2.getOrDefault(c2, 0) + 1);
             }
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (even1[i] != even2[i] || odd1[i] != odd2[i]) {
-                return false;
-            }
-        }
-
-        return true;
+        return even1.equals(even2) && odd1.equals(odd2);
     }
 }

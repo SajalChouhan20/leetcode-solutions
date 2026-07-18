a220a15240ae3fa5a511a8f2484a9d7044acc6d9
SajalChouhan20 <chouhansajal9@gmail.com>
2026-07-18 23:01:25 +0530

    Time: 6 ms (5.27%), Space: 50.6 MB (5.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,43 @@
 class Solution {
-    public void gen(int open,int close,String s,List<String> res,int n){
-        if(open==n && close==n){
-            res.add(s);
-            return;
-        }
-        if(open<n){
-            gen(open+1,close,s+"(",res,n);
-        }
-        if(close<open){
-            gen(open,close+1,s+")",res,n);
-        }
-    } 
+    // public void gen(int open,int close,String s,List<String> res,int n){
+    //     if(open==n && close==n){
+    //         res.add(s);
+    //         return;
+    //     }
+    //     if(open<n){
+    //         gen(open+1,close,s+"(",res,n);
+    //     }
+    //     if(close<open){
+    //         gen(open,close+1,s+")",res,n);
+    //     }
+    // } 
     public List<String> generateParenthesis(int n) {
-        List<String> res=new ArrayList<>();
-        gen(0,0,"",res,n);
-        return res;
+        // List<String> res=new ArrayList<>();
+        // gen(0,0,"",res,n);
+        // return res;
+        List<List<String>> dp = new ArrayList<>();
+
+        dp.add(Arrays.asList(""));
+
+        for (int i = 1; i <= n; i++) {
+
+            List<String> cur = new ArrayList<>();
+
+            for (int j = 0; j < i; j++) {
+
+                for (String left : dp.get(j)) {
+
+                    for (String right : dp.get(i - 1 - j)) {
+
+                        cur.add("(" + left + ")" + right);
+
+                    }
+                }
+            }
+
+            dp.add(cur);
+        }
+
+        return dp.get(n);
     }
 }
